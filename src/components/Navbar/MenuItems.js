86dc2ce93451ab380a07317b65bfa8aa1453bf67
Hemanth-2002 <hemanthkakumanu1@gmail.com>
2022-04-14 00:03:23 +0530

    navbar

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title : 'Committee members',
+        url : '#',
+        cName : 'nav-links'
+    },
+    {
+        title : 'Add members',
+        url : '#',
+        cName : 'nav-links'
+    },
+    {
+        title : 'Schedule Meet',
+        url : '#',
+        cName : 'nav-links'
+    },
+    {
+        title : 'Event review',
+        url : '#',
+        cName : 'nav-links'
+    },
+    {
+        title : 'Update profile',
+        url : '#',
+        cName : 'nav-links'
+    }
+]
